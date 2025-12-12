f27e45edc8de82fb913d0cb34ed15f7df1bc022c
samimkabiru <kabirusamimadeiza@gmail.com>
2025-12-12 13:39:43 +0100

    Make styling changes to the News and Projects pages.

--- src/pages/Projectspage.tsx
+++ src/pages/Projectspage.tsx
@@ -78,6 +78,31 @@ const Projectspage = () => {
     );
   }
 
+  if (projects.length === 0) {
+    return (
+      <div className="max-w-[1140px] mx-auto px-4 py-12">
+        <div>
+          <h1 className="text-3xl md:text-4xl mb-2 font-semibold text-green-900">
+            we finish what we start
+          </h1>
+          <p className="text-gray-600 mb-6">
+            At the heart of the Livestock Productivity and Resilience
+            Enhancement Project (L-PRES) are impactful initiatives aimed at
+            bringing about positive change in Nigeria's livestock sector. Our
+            diverse range of projects reflects our commitment to enhancing
+            productivity, resilience, and commercialization across selected
+            value chains.
+          </p>
+        </div>
+        <div className="h-64 rounded-2xl border-dashed border-2 text-2xl text-gray-600 flex justify-center items-center border-gray-300">
+          <p className="tracking-wider font-extralight">
+            Published projects appear here...
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-[1140px] mx-auto px-4 py-12">
       <div>
